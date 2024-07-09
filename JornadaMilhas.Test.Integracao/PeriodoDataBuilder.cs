4ebf77267fff21ee6a62b1bf2ea40fd4b26efdee
Caiera03 <caiodepaula90@hotmail.com>
2024-07-09 16:16:33 -0300

    Módulo 5: conclusão

---
+++ JornadaMilhas.Test.Integracao/PeriodoDataBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Bogus;
+using JornadaMilhasV1.Modelos;
+
+namespace JornadaMilhas.Test.Integracao;
+public class PeriodoDataBuilder : Faker<Periodo>
+{
+    public DateTime? DataInicial { get; set; }
+    public DateTime? DataFinal { get; set; }
+
+    public PeriodoDataBuilder()
+    {
+       CustomInstantiator(f =>
+            {
+                DateTime dataInicial = DataInicial ?? f.Date.Soon();
+                DateTime dataFinal = DataFinal ?? dataInicial.AddDays(30);
+                return new Periodo(dataInicial, dataFinal);
+            });
+    }
+
+    public Periodo Build() => Generate();
+
+}
